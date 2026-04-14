f52d743b6e1cd91a00de1a420057ca75e5dcac2d
J1umpy <limingyou5@gmail.com>
2026-04-14 16:52:39 -0400

    Modified values to trigger test failures.

--- ProductsApp/ProductsApp/Controllers/ProductsController.cs
+++ ProductsApp/ProductsApp/Controllers/ProductsController.cs
@@ -11,9 +11,9 @@ namespace ProductsApp.Controllers
     {
         Product[] products = new Product[]
         {
-            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
-            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+            new Product { Id = 1, Name = "Tomato", Category = "Fruit", Price = 3 },
+            new Product { Id = 2, Name = "Web", Category = "Spider", Price = 2.3M },
+            new Product { Id = 3, Name = "Humvee", Category = "Car", Price = 86.86M }
         };
 
         public ProductsController()
